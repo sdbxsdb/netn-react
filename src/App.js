5ee576332413f18ed69e430da73a8ef061e97be0
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-28 14:31:34 +0100

    part 5 starting

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog"
+  const likes = 50;
+  const link = "https://google.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+
+        <p>{ 10 }</p>
+        <p>{ "Hello string" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={link}>Google</a>
       </div>
     </div>
   );
