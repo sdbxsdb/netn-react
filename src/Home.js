0b2f5c43ec8f6b3320349e55ef0a6588cc85ae4f
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-28 17:45:06 +0100

    before starting part 13

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,13 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'jimmy', id: 3 }
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
 
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title={"All Blogs!"} />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"Marios Blogs"} />
     </div>
   );
 }
