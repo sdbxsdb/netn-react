44372bf272a3768f5f4a24115c7626dbf39a49e1
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-28 17:58:39 +0100

    before starting part 15

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -14,6 +14,11 @@ const Home = () => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
+
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  });
 
   return ( 
     <div className="home">
