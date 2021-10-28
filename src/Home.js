404f99c44be056e8f89cb0cb7b0f6484ccf3e8d7
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-28 18:03:55 +0100

    update part 14

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -9,10 +9,20 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
 
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  });
+
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title={"All Blogs!"} />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"Marios Blogs"} />
+      <BlogList blogs={blogs} title={"All Blogs!"} handleDelete={handleDelete} />
     </div>
   );
 }
