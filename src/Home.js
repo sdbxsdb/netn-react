527bcdc4fb50beed7eb7b96338d61b101d4806f0
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-29 10:00:49 +0100

    before starting part 19

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,27 @@ import BlogList from './BlogList';
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
+
+  // setTimeout function is only to simulate loading the data in because ours comes in so quickly
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
       .then(res => {
         return res.json()
       })
       .then((data) => {
         setBlogs(data);
+        setIsLoading(false);
       });
+    }, 1000);
   }, []);
 
   return ( 
     <div className="home">
-      {/* If blogs is null (false) it won't get past the && and wont do the next part - if there is data in the blogs it will move to the next step after the && and output the blogs without throwing and error */}
+      { isLoading && <div>Loading...</div> }
       { blogs && <BlogList blogs={blogs} title={"All Blogs!"} /> }
     </div>
   );
