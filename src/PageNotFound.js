1365fa9107f32ab37bcfe2f7ff6bc140470d6b79
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-29 16:40:33 +0100

    tailwind installed

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 const PageNotFound = () => {
   return ( 
     <div className="not-found">
-      <h2>Sorry!</h2>
-      <p>The page you're looking for doesn't exist</p>
-      <Link to='/'>Back to the Home Page</Link>
+      <h2 className="mb-5">Sorry!</h2>
+      <p className="mb-5">The page you're looking for doesn't exist</p>
+      <Link to='/' className='underline hover:text-red-500'>Back to the Home Page</Link>
     </div>
-   );
+  );
 }
 
 export default PageNotFound;
