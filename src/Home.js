62346a76bee8fe8e583ed629ef97ea7f0f4c175e
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-29 10:13:36 +0100

    before starting part 20

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError ] = useState(null);
 
 
   // setTimeout function is only to simulate loading the data in because ours comes in so quickly
@@ -12,18 +13,27 @@ const Home = () => {
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsLoading(false);
-      });
+        .then(res => {
+          if (!res.ok) {
+            throw new Error('Could not fetch data for that resource');
+          }
+          return res.json()
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch(error => {
+          setIsLoading(false);
+          setError(error.message);
+        })
     }, 1000);
   }, []);
 
   return ( 
     <div className="home">
+      { error && <div>Error - {error}</div>}
       { isLoading && <div>Loading...</div> }
       { blogs && <BlogList blogs={blogs} title={"All Blogs!"} /> }
     </div>
