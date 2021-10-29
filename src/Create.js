3b804dc61ae8f4a76f7c869ea49cfa3aacb25a4a
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-29 11:05:59 +0100

    before starting part 24

--- src/Create.js
+++ src/Create.js
@@ -3,7 +3,7 @@ const Create = () => {
     <div className="create">
       <h2>Add a New Blog</h2>
     </div>
-   );
+  );
 }
 
 export default Create;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return ( 
     <nav className="navbar">
-      <a href="/">
+      <Link to="/">
         <h1>The Blog</h1>
-      </a>
+      </Link>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </nav>
   );
